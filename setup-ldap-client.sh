d8a0b9df2dbc3d727e65f26fd8c7300b785f8b10
Mark Cunningham <markcunninghamemail@gmail.com>
2012-01-28 01:42:10 +0000

    copy conf files to /etc + chmod

--- setup-ldap-client.sh
+++ setup-ldap-client.sh
@@ -8,21 +8,19 @@ cp -r $configsDir/ldap $configsTempDir
 
 debconf-set-selections ldap/configs/debconf-defaults
 apt-get install libpam-ldapd 
-chmod 700 /etc/ldap/ldap.conf
 
 
 
 
-#Install packages
-system("apt-get install libpam-ldapd") ;
-system("chmod 700 /etc/ldap/ldap.conf");
-system("apt-get install sudo-ldap rcconf");
-system("rcconf -off nscd");
-system("/etc/init.d/nscd stop");
 
+systemHostname=`hostname`
 
-echo "Enter Hostname: "
+echo "Enter Hostname: empty for ($systemHostname)"
 read hostname
+
+if [ -z $hostname ]; then
+	hostname=$systemHostname
+fi
 
 echo "Nslcd password: "
 read nslcdPassword
@@ -35,5 +33,14 @@ read sudoPassword
 
 #Setup configs
 sed -i 's/%hostname%/$hostname/g' $configsTempDir/ldap/*
-sed -i 's/%sudopassword%/$sudoPassword/g' $configsTempDir/ldap/*
-sed -i 's/%nslcdpassword%/$nslcdPassword/g' $configsTempDir/ldap/*
+sed -i 's/%sudopassword%/$sudoPassword/g' $configsTempDir/ldap/ldap.conf
+sed -i 's/%nslcdpassword%/$nslcdPassword/g' $configsTempDir/ldap/nslcd.conf
+
+
+cp $configsTempDir/ldap/ca.crt /etc/ldap/ca.crt
+cp $configsTempDir/ldap/nslcd.conf /etc/nslcd.conf
+cp $configsTempDir/ldap/ldap.conf /etc/ldap/ldap.conf
+
+
+chmod 700 /etc/ldap/ldap.conf
+chmod 700 /etc/nslcd.conf
