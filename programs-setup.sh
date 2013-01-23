46e0adce6fabb68c7ce63c7486dbc7dcbf887216
Mark Cunningham <contact@markcunningham.ie>
2013-01-23 01:53:22 +0000

    added -y install to programs-setup.sh

--- programs-setup.sh
+++ programs-setup.sh
@@ -2,6 +2,6 @@
 
 #packages nmap hping3 dnsutils vim less 
 sudo apt-get update
-sudo apt-get install less vim hping3 nmap dnsutils byobu
+sudo apt-get -y install less vim hping3 nmap dnsutils byobu
 
 echo syntax on > ~/.vimrc
