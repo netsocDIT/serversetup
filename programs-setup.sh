57d917de9e57c1b2712d3e6e71cad19778911582
Mark Cunningham <contact@markcunningham.ie>
2013-01-31 18:55:41 +0000

    added root check for program-setup script and removed sudo in front of commands

--- programs-setup.sh
+++ programs-setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+if [ `id -u` -ne 0 ]; then
+	echo "Must be root to run this script"
+	exit 1
+fi
+
+
 #packages nmap hping3 dnsutils vim less 
-sudo apt-get update
-sudo apt-get -y install less vim hping3 nmap dnsutils byobu
+apt-get update
+apt-get -y install less vim hping3 nmap dnsutils byobu
 
 echo syntax on > ~/.vimrc
